e331b3be396ea94a3c3df0b5f3c53d887d2080e2
Andre Barnes <contact@csabarnes.com>
2017-05-07 02:55:51 -0400

    4.29.17
    updated catchup study , 5.7.17

--- Lesson_5.cpp
+++ Lesson_5.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 
 
+Working with Expressions, Statements, and Operators
+
+If you need to spread a statement over two lines
+you can do it by inserting a backslash (\) at the end.
+
+cout << "Hello \
+	 World" << endl; // split to two lines is OK
+or
+
+cout << "Hello "
+	"World" << endl; // two string literals is OK
+
+
+Block : a group of Statements
+
+r - values : values to the right of equal sign ; content of memory location
+l - values : values to the left of equal sign ; locations in memory
+
+
